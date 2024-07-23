70a84cfa805f01b453775c52533fa30847299395
Atlas Dev Labs <168639806+AtlasDevelopmentLabs@users.noreply.github.com>
2024-07-23 21:32:17 +1200

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'Next Dev Labs Edited by Atlas Development Labs'
+description 'Free Standalone Duty System'
+version '2.0.0'
+
+shared_script 'config.lua'
+server_script 'server.lua'
