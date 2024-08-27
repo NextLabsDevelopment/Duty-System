a9c81b4edc31fc6be752f99992314dfc47a258ad
Luke K. <144915098+NextLabsDevelopment@users.noreply.github.com>
2024-08-27 23:41:06 +0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,12 @@
 fx_version 'cerulean'
 games { 'gta5' }
-author 'Next Dev Labs Edited by Atlas Development Labs'
+
+author 'Next Dev Labs'
 description 'Free Standalone Duty System'
 version '2.0.0'
 
 shared_script 'config.lua'
+
 server_script 'server.lua'
+
+client_script 'client.lua'
