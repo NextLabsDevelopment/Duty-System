29808393777b7bf36ad74cb94271669167731e0e
Next Dev Labs <144915098+NextLabsDevelopment@users.noreply.github.com>
2024-08-28 00:48:11 +0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,10 +3,8 @@ games { 'gta5' }
 
 author 'Next Dev Labs'
 description 'Free Standalone Duty System'
-version '2.0.0'
+version '1.1'
 
 shared_script 'config.lua'
-
 server_script 'server.lua'
-
 client_script 'client.lua'
